5ab66bda7fd37aaf0a97cca1ed78e643b74dac86
YasuhiroKimesawa <kimesawa_yasuhiro@aratana.jp>
2018-04-19 00:19:03 +0900

    modify set cron

--- ansible/roles/files/run_wercker.sh
+++ ansible/roles/files/run_wercker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -X POST -H "Authorization: Bearer ${WERCKER_TOKEN}" -H 'Content-type: application/json' https://app.wercker.com/api/v3/runs -d "{\"pipelineId\":\"${WERCKER_PIPELINE_ID}\",\"branch\": \"master\"}"
+curl -X POST -H "Authorization: Bearer {{WERCKER_TOKEN}}" -H 'Content-type: application/json' https://app.wercker.com/api/v3/runs -d "{\"pipelineId\":\"{{WERCKER_PIPELINE_ID}}\",\"branch\": \"master\"}"
