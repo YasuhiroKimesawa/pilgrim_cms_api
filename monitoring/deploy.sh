e4bbdad4955c7bcf6d5105b8410a42402b77f281
YasuhiroKimesawa <kimesawa_yasuhiro@aratana.jp>
2018-05-02 02:14:11 +0900

    modify wercker

--- monitoring/deploy.sh
+++ monitoring/deploy.sh
@@ -3,23 +3,23 @@
 cwd=$(pwd)
 deploy_bucket='pilgrim-crm-monitoring-app-deployment'
 
-mkdir -p $(pwd)/dist
-rm -rf $(pwd)/dist/*
-cp -r ${cwd}/app ${cwd}/dist/app
-cp ${cwd}/template.yml ${cwd}/dist/template.yml
+mkdir -p $(pwd)/monitoring/dist
+rm -rf $(pwd)/monitoring/dist/*
+cp -r ${cwd}/monitoring/app ${cwd}/monitoring/dist/app
+cp ${cwd}/monitoring/template.yml ${cwd}/monitoring/dist/template.yml
 
 if aws s3 ls s3://${deploy_bucket} --region ap-northeast-1 --profile ${AWS_PROFILE} 2>&1 | grep -q 'NoSuchBucket'; then
     aws s3 mb s3://${deploy_bucket} --region ap-northeast-1 --profile ${AWS_PROFILE}
 fi
 
 aws cloudformation package \
-    --template-file ${cwd}/dist/template.yml \
+    --template-file ${cwd}/monitoring/dist/template.yml \
     --s3-bucket ${deploy_bucket} \
-    --output-template-file ${cwd}/dist/packaged-template.yml \
+    --output-template-file ${cwd}/monitoring/dist/packaged-template.yml \
     --profile ${AWS_PROFILE}
 
 aws cloudformation deploy \
-    --template-file ${cwd}/dist/packaged-template.yml \
+    --template-file ${cwd}/monitoring/dist/packaged-template.yml \
     --stack-name pilgrim-crm-monitoring \
     --capabilities CAPABILITY_IAM \
     --profile ${AWS_PROFILE} \
