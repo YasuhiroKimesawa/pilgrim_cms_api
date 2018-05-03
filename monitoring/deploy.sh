ee99af446d3df5ad4ab127b4545760582d5239c5
YasuhiroKimesawa <kimesawa_yasuhiro@aratana.jp>
2018-05-04 01:08:46 +0900

    coverage result s3

--- monitoring/deploy.sh
+++ monitoring/deploy.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-cwd=$(pwd)
+cwd=$(pwd)/monitoring
 deploy_bucket='pilgrim-crm-monitoring-app-deployment'
 
-mkdir -p $(pwd)/monitoring/dist
-rm -rf $(pwd)/monitoring/dist/*
-cp -r ${cwd}/monitoring/app ${cwd}/monitoring/dist/app
-cp ${cwd}/monitoring/template.yml ${cwd}/monitoring/dist/template.yml
+mkdir -p ${cwd}/dist
+rm -rf ${cwd}/dist/*
+cp -r ${cwd}/app ${cwd}/dist/app
+cp ${cwd}/template.yml ${cwd}/dist/template.yml
 
 if aws s3 ls s3://${deploy_bucket} --region ap-northeast-1 --profile ${AWS_PROFILE} 2>&1 | grep -q 'NoSuchBucket'; then
     aws s3 mb s3://${deploy_bucket} --region ap-northeast-1 --profile ${AWS_PROFILE}
 fi
 
-aws cloudformation package \
-    --template-file ${cwd}/monitoring/dist/template.yml \
-    --s3-bucket ${deploy_bucket} \
-    --output-template-file ${cwd}/monitoring/dist/packaged-template.yml \
-    --profile ${AWS_PROFILE}
+(cd ${cwd};pip install -r requirements.txt -t ${cwd}/dist)
 
-aws cloudformation deploy \
-    --template-file ${cwd}/monitoring/dist/packaged-template.yml \
+(cd ${cwd};aws cloudformation package \
+    --template-file ${cwd}/dist/template.yml \
+    --s3-bucket ${deploy_bucket} \
+    --output-template-file ${cwd}/dist/packaged-template.yml \
+    --profile ${AWS_PROFILE})
+
+(cd ${cwd};aws cloudformation deploy \
+    --template-file ${cwd}/dist/packaged-template.yml \
     --stack-name pilgrim-crm-monitoring \
     --capabilities CAPABILITY_IAM \
     --profile ${AWS_PROFILE} \
-    --parameter-overrides StageName=dev SlackWebhookURL=${SLACK_WEBHOOK_URL} HealthCheckURL=${HEALTH_CHECK_URL}
+    --parameter-overrides StageName=dev SlackWebhookURL=${SLACK_WEBHOOK_URL} HealthCheckURL=${HEALTH_CHECK_URL})
