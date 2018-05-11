4e40d4f9c4c15febd3ad0f0337f8292651ac66c5
YasuhiroKimesawa <kimesawa_yasuhiro@aratana.jp>
2018-05-11 19:52:58 +0900

    modify list object for s3

--- monitoring/app/notify_stress_test.py
+++ monitoring/app/notify_stress_test.py
@@ -31,8 +31,8 @@ def build_message(s3_bucket_name, web_site_url):
     post_message = '負荷テストが完了しました¥n'
     s3client = boto3.client('s3')
     list_objects = s3client.list_objects_v2(Bucket=s3_bucket_name, Prefix='/', Delimiter='/')
-    for folder in list_objects.get('CommonPrefixes'):
-        post_message = post_message + (folder.get('Prefix') + "index.html¥n")
+    for folder in list_objects["Contents"]:
+        post_message = post_message + (folder["Key"] + '/' + "index.html¥n")
 
     side_color = "good"
 
